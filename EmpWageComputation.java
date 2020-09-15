20b3d95dccb9aef0ff557c02870d5214547f669f
shilpa <shilpa07udupi@gmail.com>
2020-09-15 13:34:31 +0530

    Adding employee attendence

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,5 +3,11 @@ public class EmpWageComputation
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to employee wage computation problem !");
+        	int random=(int)Math.floor(Math.random()*10)%2;
+		
+		if(random == 1)
+		System.out.println("Employee is present");
+		else
+		System.out.println("Employee is absent");
 	}
 }
