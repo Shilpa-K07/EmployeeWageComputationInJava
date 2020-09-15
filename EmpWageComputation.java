0ace301baef9fa29d7985422b97b06bd96cd5100
shilpa <shilpa07udupi@gmail.com>
2020-09-15 14:16:11 +0530

    Calculating wage for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,26 +3,32 @@ public class EmpWageComputation
 	public static final int WAGEPERHOUR=20;
 	public static final int FULLDAYHOUR=8;
 	public static final int PARTTIMEHOUR=4;
+	public static final int	TOTALWORKINGDAYS=20;
 	public static void main(String[] args)
 	{
-		int dailyWage=0;
+		int totalWage=0;
 		int workingHour=0;
+		int i=0;
 		System.out.println("Welcome to employee wage computation problem !");
-        	int random=(int)Math.floor(Math.random()*10)%3;
 
-		 switch(random)
-                {
-                        case 1: 
-			workingHour=FULLDAYHOUR;
-                        break;
-                        case 2: 
-			workingHour=PARTTIMEHOUR;
-                        break;
-                        default:       
-			workingHour=workingHour;
-                        break;
-                }
-		dailyWage=workingHour*WAGEPERHOUR;
-		System.out.println("Daily Wage is:" +dailyWage);
+		 while(i<TOTALWORKINGDAYS)
+		{
+			int random=(int)Math.floor(Math.random()*10)%3;                        
+		 	switch(random)
+         	{
+            	case 1: 
+					workingHour=FULLDAYHOUR;
+               break;
+               case 2: 
+					workingHour=PARTTIMEHOUR;
+               break;
+               default:       
+					workingHour=0;
+               break;
+				}
+			i++;
+			totalWage+=workingHour*WAGEPERHOUR;
+		 }
+		System.out.println("Wage for a month:" +totalWage);
 	}
 }
