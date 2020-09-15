ffc5398f2834597bb84b21ec4cb500541d0fc34f
shilpa <shilpa07udupi@gmail.com>
2020-09-15 13:47:43 +0530

    Calculating daily wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,13 +1,14 @@
 public class EmpWageComputation
 {
+	public static final int WAGEPERHOUR=20;
+	public static final int FULLDAYHOUR=8;
 	public static void main(String[] args)
 	{
+		int dailyWage=0;
 		System.out.println("Welcome to employee wage computation problem !");
         	int random=(int)Math.floor(Math.random()*10)%2;
-		
-		if(random == 1)
-		System.out.println("Employee is present");
-		else
-		System.out.println("Employee is absent");
+		if(random ==1)
+		dailyWage=WAGEPERHOUR*FULLDAYHOUR;
+		System.out.println("Daily Wage is:" +dailyWage);
 	}
 }
