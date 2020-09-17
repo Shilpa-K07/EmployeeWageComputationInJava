e782a92334f086d2fab7054847684dc8d366093f
shilpa <shilpa07udupi@gmail.com>
2020-09-17 16:18:03 +0530

    Adding UC8

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.util.Scanner;
 public class EmpWageComputation
 {
 	public static final int WAGEPERHOUR=20;
@@ -67,7 +69,20 @@ public class EmpWageComputation
                  }
                 System.out.println("Total Wage of "+company+" is : " +totalWage);
         }
-		
+	public static ArrayList getDetails()
+	{
+		Scanner scn=new Scanner(System.in);
+		ArrayList arr=new ArrayList();
+		System.out.println("Enter company name");
+		arr.add(scn.next());
+		System.out.println("Enter Wage per hour");
+		arr.add(scn.nextInt());
+		System.out.println("Enter total working days");
+		arr.add(scn.nextInt());
+		System.out.println("Enter total working hours");
+		arr.add(scn.nextInt());
+		return arr;
+	}	
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to employee wage computation problem !");
@@ -159,7 +174,15 @@ public class EmpWageComputation
 		System.out.println("Wage calculation using class method");
 		calculateEmpWage();
 		System.out.println("Wage Calculation for multiple companies");
-		calculateEmpWage("Oracle",40,20,100);
-		calculateEmpWage("Intel",30,20,200);
+		Scanner scn =new Scanner(System.in);
+		System.out.println("Enter the number of companies");
+		int n=scn.nextInt();
+		int j=0;
+		while(j<n)
+		{
+			ArrayList arr=getDetails();
+			calculateEmpWage(arr.get(0).toString(),(int)arr.get(1),(int)arr.get(2),(int)arr.get(3));
+			j++;
+		}
 	}
 }
